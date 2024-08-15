8e3233fa32164275e155b4fac4186fa1c9114033
bkallaus <benkallaus@gmail.com>
2024-08-14 21:25:18 -0500

    feat: pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,53 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { Toaster } from '@/components/ui/toaster';
-import Header from '@/components/header';
-import { hasAccess } from '@/server-actions/verify-credentials';
-const inter = Inter({ subsets: ['latin'] });
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import Header from "@/components/header";
+import { hasAccess } from "@/server-actions/verify-credentials";
+import type { Metadata, Viewport } from "next";
+const inter = Inter({ subsets: ["latin"] });
+
+const APP_NAME = "Smart Recipe";
+const APP_DEFAULT_TITLE = "Smart Recipe";
+const APP_TITLE_TEMPLATE = "%s - Smart Recipe";
+const APP_DESCRIPTION = "Recipe app for organizing your recipes";
 
 export const metadata: Metadata = {
-  title: 'Smart Recipes',
-  description: 'Smart Recipe ingests and organizes your recipes',
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default async function RootLayout({
@@ -18,10 +57,11 @@ export default async function RootLayout({
 }>) {
   if (!(await hasAccess())) {
     return (
-      <html lang='en'>
+      <html lang="en">
+        <head />
         <body className={inter.className}>
           <Header />
-          <div className='m-auto text-xl font-bold text-center'>
+          <div className="m-auto text-xl font-bold text-center">
             Please Login to Access Site
           </div>
         </body>
@@ -30,7 +70,8 @@ export default async function RootLayout({
   }
 
   return (
-    <html lang='en'>
+    <html lang="en">
+      <head />
       <body className={inter.className}>
         <Header />
         {children}
